e43e29b2abb9d5af4badaf311a48866d5a096695
Shyam Mohan Gupta <shyamprime2610@gmail.com>
2024-06-06 22:23:13 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className={styles.main_header}> 
      <div className={styles.navbar_brand}>
             <Link href='/' >
-                <Image src="/logo.png" alt="my logo image" width={350} height={140}/>
+                <Image src="/logo.png" alt="my logo image" width={250} height={40}/>
 
             </Link>
             </div>
