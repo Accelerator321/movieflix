977838a5dbea639db729f9451d891c9c00e69c29
Shyam Mohan Gupta <shyamprime2610@gmail.com>
2024-06-12 19:05:42 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
-import Head from 'next/head'
+
 
 export default function main() {
   return (
     <>
-      <Head>
-        // <meta name="viewport" content="viewport-fit=cover" />
-     <meta name={"viewport"} content={{width:{"device-width"}, initialScale:{1.0}, maximumScale:{1.0}, userScalable:{0}}} />
-      </Head>
+   
 
     <HeroSection title={"LETS'S WATCH MOVIE TOGETHER"} imageUrl={"/about1.svg"}/>
     </>
