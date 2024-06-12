a962b3e1bd995c5bae9f8c2a458d9f09f55fbff8
Shyam Mohan Gupta <shyamprime2610@gmail.com>
2024-06-12 18:58:15 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function main() {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        // <meta name="viewport" content="viewport-fit=cover" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </Head>
 
     <HeroSection title={"LETS'S WATCH MOVIE TOGETHER"} imageUrl={"/about1.svg"}/>
